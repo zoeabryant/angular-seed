9595233bdf74bb5f71f3673090a137dc0e3d8e29
Zoe Bryant <z.bryant@kainos.com>
2015-11-05 16:53:02 +0000

    Add, view, delete friends link with blackbox - refactoring

--- app/viewFriends/viewFriends.js
+++ app/viewFriends/viewFriends.js
@@ -12,30 +12,26 @@ angular.module('myApp.viewFriends', ['ngRoute'])
 .controller('ViewFriendsCtrl', ['$scope', 'NameService', 'SessionService', 'FriendService', function($scope, NameService, SessionService, FriendService) {
 
   SessionService.kickOutIfNotLoggedIn();
-  
+
   var loadData = function(){
-    FriendService.getFriends(getFriendsSuccessCallback, errorCallback);
+    FriendService.getFriends(getFriendsSuccessCallback, genericErrorCallback);
   }
 
   var getFriendsSuccessCallback = function(response) {
     $scope.friends = response.data;
   }
 
-  var errorCallback = function(response) {
+  var genericErrorCallback = function(response) {
     console.log('Something has gone wrong, please try again later');
   }
 
-  var reloadOnSuccessCallback = function(response) {
-    loadData();
-  }
-
   $scope.addFriend = function(){
-    FriendService.addFriend($scope.addFriendForm.name, reloadOnSuccessCallback, errorCallback);
+    FriendService.addFriend($scope.addFriendForm.name, loadData, genericErrorCallback);
   };
 
   $scope.deleteFriend = function(id){
     console.log('deleteFriend clicked');
-    FriendService.deleteFriend(id, reloadOnSuccessCallback, errorCallback);
+    FriendService.deleteFriend(id, loadData, genericErrorCallback);
   };
 
   loadData();
